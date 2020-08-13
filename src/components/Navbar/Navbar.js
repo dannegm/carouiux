1644f70bd34e418e5f47c78521211a80c2ede68a
Designer <designer@MBP-17-3467.local>
2020-08-12 23:45:53 -0500

    navbar,skills,projects

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-import { NavBar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 
 export default function NavBar() {
   return (
-    <NavBar bg="light">
+    <Navbar bg="light">
 
 
       <NavBar.Brand href="/" className="ml-5">Carolina Sánchez Guzmán</NavBar.Brand>
-        <NavBar className="ml-auto">
+        <Navbar className="ml-auto">
             <Nav.Link href="#" className="mt-2 mb-2 ml-4 mr-4">Skills</Nav.Link>
             <Nav.Link href="#" className="mt-2 mb-2 ml-4 mr-4">Projects</Nav.Link>
             <Nav.Link href="#" className="mt-2 mb-2 ml-4 mr-4">About</Nav.Link>
 
-        </NavBar>
-
+        </Navbar>
+        </Navbar>
 
   )
 }
